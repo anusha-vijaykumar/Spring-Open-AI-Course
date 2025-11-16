48f05654b1a2ec32d366807272baa77eea2d7351
Anusha Vijay Kumar <v.annusha@gmail.com>
2025-11-16 12:25:38 -0500

    Adding username to converstaion

--- openai/src/main/java/com/example/openai/controller/ChatMemoryController.java
+++ openai/src/main/java/com/example/openai/controller/ChatMemoryController.java
@@ -3,10 +3,9 @@ package com.example.openai.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +18,7 @@ public class ChatMemoryController {
     }
 
     @GetMapping("/chat-memory")
-    public ResponseEntity<String> chatMemory(@RequestParam("message") String message){
-        return ResponseEntity.ok(memoryChatClient.prompt().user(message).call().content());
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username, @RequestParam("message") String message){
+        return ResponseEntity.ok(memoryChatClient.prompt().user(message).advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, username)).call().content());
     }
 }
